cb26cb3e608424eff17c9727c095cb9f52a19305
windiam <jianfeng129@163.com>
2016-01-22 11:51:35 +0800

    提交

--- gb-admin-core/src/main/java/com/genebook/admin/service/SysUserDubboService.java
+++ gb-admin-core/src/main/java/com/genebook/admin/service/SysUserDubboService.java
@@ -1,7 +1,15 @@
 package com.genebook.admin.service;
 
 import com.genebook.auth.dto.SysUserDTO;
-
+/**
+ * 后台管理员Service层
+ * @description
+ *
+ * @author   wind
+ * @version  [1.0, 2016年1月22日]
+ * @see      [相关类/方法]
+ * @since    [产品/模块版本]
+ */
 public interface SysUserDubboService {
 
 	public SysUserDTO queryByUsername(String username);
